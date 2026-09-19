a3ea492217cb4ea2fd66638f8275cbbd6c70f43a
Дмитрий Толстошеев <det9@tpu.ru>
2026-09-19 13:37:03 +0700

    Добавлен новый эндпоинт /about

--- src/app.js
+++ src/app.js
@@ -5,4 +5,8 @@ app.get('/health', (req, res) => {
     res.status(200).json({status: 'OK'});
 })
 
+app.get('/about', (req, res) => {
+    res.status(200).json({about: 'lab1'});
+});
+
 module.exports = app;
